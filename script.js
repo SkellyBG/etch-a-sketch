4be4519303769f3b60090aec88f4cf0916612032
SkellyBG <skellybg245@gmail.com>
2022-10-03 13:45:32 +1100

    Add size prompt

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+// const GRID_LENGTH = 640;
+
 const createGrid = function createGrid(size) {
   const container = document.querySelector(".container");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
   for (let i = 0; i < size; i++) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
@@ -11,11 +16,26 @@ const createGrid = function createGrid(size) {
     }
     container.appendChild(rowDiv);
   }
-}
+};
 
 const hover = function hover(node) {
   node.classList.add("colored");
-}
+};
 
+const sizePrompt = function sizePrompt() {
+  const size = Number.parseInt(prompt("Enter size:"));
+  if (Number.isNaN(size)) {
+    alert("That's not a number!");
+  } else if (size < 1) {
+    alert("That number is too small!");
+  } else if (size > 64) {
+    alert("That number is too big!");
+  } else {
+    createGrid(size);
+  }
+};
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", sizePrompt);
 createGrid(16);
 
